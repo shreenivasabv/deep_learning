6e188a663d912e015315978038c38a66c29b036b
Shreenivasa Murthy B V <179219653+shreenivasabv@users.noreply.github.com>
2026-08-28 07:38:29 +0530

    Implement CNN for MNIST classification
    This script implements a convolutional neural network using TensorFlow to classify the MNIST dataset. It includes data preprocessing, model architecture definition, compilation, training, and evaluation.

---
+++ 6.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+
+from tensorflow.keras import layers, models
+from tensorflow.keras.datasets import mnist
+
+(X_train, y_train), (X_test, y_test) = (
+    mnist.load_data()
+)
+
+X_train = (
+    X_train.reshape(-1, 28, 28, 1)
+    / 255.0
+)
+
+X_test = (
+    X_test.reshape(-1, 28, 28, 1)
+    / 255.0
+)
+
+model = models.Sequential([
+    layers.Conv2D(
+        32,
+        (3, 3),
+        activation="relu",
+        input_shape=(28, 28, 1)
+    ),
+
+    layers.MaxPooling2D(
+        (2, 2)
+    ),
+
+    layers.Conv2D(
+        64,
+        (3, 3),
+        activation="relu"
+    ),
+
+    layers.MaxPooling2D(
+        (2, 2)
+    ),
+
+    layers.Flatten(),
+
+    layers.Dense(
+        64,
+        activation="relu"
+    ),
+
+    layers.Dense(
+        10,
+        activation="softmax"
+    )
+])
+
+model.compile(
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.fit(
+    X_train,
+    y_train,
+    epochs=5,
+    batch_size=64
+)
+
+print(
+    "Accuracy:",
+    model.evaluate(X_test, y_test)[1]
+)
